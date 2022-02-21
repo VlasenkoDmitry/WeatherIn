6a13a10ddfd03d4b2831c2971c9f8903b706d723
Dmitry Vlasenko <vlasenko.dmitry1@gmail.com>
2022-02-21 12:04:08 +0300

    add project

---
+++ WeatherIn/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  WeatherIn
+//
+//  Created by Ap on 21.02.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
